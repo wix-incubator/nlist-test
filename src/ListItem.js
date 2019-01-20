27febdbacdc8b4279554154ab977d1b6254bd013
Evgeni Silman <evgenis@wix.com>
2019-01-20 11:38:12 +0200

    We should use TouchableOpacity instead of Button.

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,23 +1,21 @@
 import React, {Component} from 'react';
-import {Text, View, Image, Button} from "react-native";
+import {Text, View, Image, TouchableOpacity} from "react-native";
 
 export default class ListItem extends Component<Props> {
     static name = "ListItem";
 
     static height = 240;
-    shouldComponentUpdate() {
-        return false;
-    }
-
-    doNothing() {}
 
     render() {
         return (<View>
-            <Button title="Press me!" nativeID="button" onPress={() => this.doNothing()} testID={"button"}/>
+            <TouchableOpacity nativeID="button">
+                <Text style={{height: 30, backgroundColor: "#00CCFF", textAlign: "center", fontSize: 18}}>
+                    Press me!
+                </Text>
+            </TouchableOpacity>
             <Text style={{height: 60, backgroundColor: "#dddddd"}} nativeID="content">TEST</Text>
-            <Image style={{width:100,height:100, backgroundColor:"#ff0000"}} nativeID="image"/>
+            <Image style={{width: 100, height: 100, backgroundColor: "#ff0000"}} nativeID="image"/>
         </View>);
     }
 
-    // <Image source={{uri: "https://facebook.github.io/react-native/img/header_logo.png"}}/>
 }
